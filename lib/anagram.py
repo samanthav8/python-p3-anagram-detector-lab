c3fa34009f45340fb26bfea08a56a89507026770
Samantha <samanthavillafana8@gmail.com>
2024-07-13 15:18:12 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        def is_anagram(word1, word2):
+            return sorted(word1) == sorted(word2)
+        
+        return [word for word in possible_anagrams if is_anagram(self.word, word)]
